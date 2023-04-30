a5b208f104749262056db181e5356dba3d7a737b
johanage <j.f.agerup@fys.uio.no>
2023-04-30 22:59:57 +0000

    next step is to split up grid in two partitions and let each gpu work on one of the partitions. Current problem is to make sure I have the correct understanding of what is height and width and also to make the program correct for arbitrary weight and height not just a square grid.

--- dual-gpu_stencil_uva.cu
+++ dual-gpu_stencil_uva.cu
@@ -9,10 +9,10 @@
 #define GPU1 0
 #define GPU2 1
 
-__global__ void stencil_kernel(float *grid, int width, int height, int sh) {
+__global__ void stencil_kernel(float *grid, int width, int height, int sw) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
-    if (i > sh && i < width - 1 && j > 0 && j < height - 1)
+    if (i > sw && i < width-1 && j > 0 && j < height-1)
     {
         // Compute the new value of the grid point (i, j)
         float new_value = (grid[(i-1)*height+j] + grid[(i+1)*height+j]
@@ -23,15 +23,17 @@ __global__ void stencil_kernel(float *grid, int width, int height, int sh) {
 }
 
 int main() {
-    const int width = 16;
+    const int width  = 8;
     const int height = 16;
     const int num_iterations = 1;
     const int block_size = 4;
     
     // Allocate memory for the grid on both GPUs using cudaMallocManaged()
     float *grid1, *grid2;
-    cudaMallocManaged(&grid1, width*height*sizeof(float));
-    cudaMallocManaged(&grid2, width*height*sizeof(float));
+    // allocate using malloc managed
+    int gridsize = (int)ceil(width*height/2); // remove div. by 2
+    cudaMallocManaged(&grid1, gridsize*sizeof(float));
+    cudaMallocManaged(&grid2, gridsize*sizeof(float));// remove div. by 2;
     
     // Initialize the grid values on GPU 1
     //printf(" Init grid1: \n");
@@ -45,7 +47,8 @@ int main() {
 	    }
             if(i >= int(width/2)-1)
 	    {
-		grid2[i*height + j] = 1;
+		int ind = (i*height + j)%gridsize;
+                grid2[ind] = 1;
 	    }
 	}
     }
@@ -53,46 +56,78 @@ int main() {
     // Launch the kernel on both GPUs
     // threads per block
     dim3 block(block_size, block_size);
-    int nx = (int)ceil(width/block.x);
+    int nx = (int)ceil(width /block.x);
     int ny = (int)ceil(height/block.y);
     dim3 grid1_size( nx, ny );
     dim3 grid2_size( nx, ny );
 
     // prefetching to device
     cudaSetDevice(GPU1);
-    cudaMemPrefetchAsync(grid1, width*height*sizeof(int), GPU1, NULL);
+    cudaMemPrefetchAsync(grid1, gridsize*sizeof(int), GPU1, NULL);
     cudaSetDevice(GPU2);
-    cudaMemPrefetchAsync(grid2, width*height*sizeof(int), GPU1, NULL);
+    cudaMemPrefetchAsync(grid2, gridsize*sizeof(int), GPU2, NULL);
 
     // enable PtoP communication
-    cudaDeviceEnablePeerAccess(GPU1, 0);
+    int is_able;
+    // check if GPU2 can access GPU1
+    cudaDeviceCanAccessPeer(&is_able, GPU2, GPU1);
+    if(is_able){
+        cudaDeviceEnablePeerAccess(GPU1, 0);
+    }
+    // check if GPU1 can access GPU2 if true enable access
+    cudaDeviceCanAccessPeer(&is_able, GPU1, GPU2);
     cudaSetDevice(GPU1);
-    cudaDeviceEnablePeerAccess(GPU2, 0);
-
+    if(is_able){
+        cudaDeviceEnablePeerAccess(GPU2, 0);
+    }
 
     for (int iter = 0; iter < num_iterations; ++iter) {
         // compute first half on GPU1 
 	//stencil_kernel<<<grid1_size, block>>>(grid1, width, int(height/2), 0);
-	stencil_kernel<<<grid1_size, block>>>(grid1, int(width/2)+1, height, 0 );
-	cudaSetDevice(GPU2);
+	stencil_kernel<<<grid1_size, block>>>(grid1,width, int(height/2), 0);
+	//cudaSetDevice(GPU2);
         // synchronize with GPU2
-	cudaDeviceSynchronize();
+	//cudaDeviceSynchronize();
 	// compute other half on GPU2
-	stencil_kernel<<<grid2_size, block>>>(grid2, width, height,int(height/2)-1);
-        cudaDeviceSynchronize();
+	//stencil_kernel<<<grid2_size, block>>>(grid2,int(width/2), height, 0);
+        //cudaDeviceSynchronize();
     }
-    // copy from GPU to print results on CPU
-    int size_grid = int(width*height); 
-    printf("Size of grid %i ", size_grid);
-    float *grid_1   = new float[size_grid]();
-    float *grid_2 = new float[size_grid]();
-    cudaMemcpy(grid_2, grid2, size_grid*sizeof(int), cudaMemcpyDeviceToHost);
+    // copy from GPU to print results on CPU using memcpy
+    // here the GPUs have the full size grid and only
+    // work on half of the domain
+    float *grid_1 = new float[gridsize]();
+    float *grid_2 = new float[gridsize]();
+    cudaMemcpy(grid_2, grid2, gridsize*sizeof(int), cudaMemcpyDeviceToHost);
     // was on GPU2 
     cudaSetDevice(GPU1);
-    cudaMemcpy(grid_1, grid1, size_grid*sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(grid_1, grid1, gridsize*sizeof(int), cudaMemcpyDeviceToHost);
+    
     // Free the memory using cudaFree()
     cudaFree(grid1);
     cudaFree(grid2);
+
+    // here GPUs have half sized grids
+    // so results needs to be stiched together when copying from devices to host
+    int size_grid = int(width*height); 
+    float *grid = new float[size_grid];
+    for (int i = 0; i < int( width ); i++)
+    {
+        for (int j = 0; j < int( height ); j++)
+        {
+            if(i <= int(width/2))
+            {
+                grid[i*height + j] = grid_1[i*height + j];
+            }
+            if(i >= int(width/2)-1)
+            {
+		//int ind = (i*height + j)%gridsize;
+                //grid[i*height + j] = grid_2[ind];
+            }
+        }
+    }
+
+
+    // print results
     printf("Printing the output of the 2D stencil example\n");
     for(int i = 0; i < width; i++)
     {
@@ -101,7 +136,7 @@ int main() {
 		//std::cout << i*width + j << " : " << grid_2[i*width + j] << " "; // << std::endl;
 		//std::cout << grid_1[i*width + j] << " "; // << std::endl;
 		//std::cout << grid_2[i*width + j] << " "; // << std::endl;
-		std::cout << grid_1[i*width + j] + grid_2[i*width + j] << " "; // << std::endl;
+		std::cout << grid[i*width + j] << " "; // << std::endl;
 	}
 	std::cout << std::endl;
     }
